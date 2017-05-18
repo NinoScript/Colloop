43ba382629f233cbac8d4edfa3198f96ed7fbf99
Maxim Zaks <maxim.zaks@gmail.com>
2017-05-18 12:01:47 +0200

    Initial Commit

---
+++ Colloop/Colloop.h
@@ -0,0 +1,19 @@
+//
+//  Colloop.h
+//  Colloop
+//
+//  Created by Maxim Zaks on 18.05.17.
+//  Copyright © 2017 Maxim Zaks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Colloop.
+FOUNDATION_EXPORT double ColloopVersionNumber;
+
+//! Project version string for Colloop.
+FOUNDATION_EXPORT const unsigned char ColloopVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Colloop/PublicHeader.h>
+
+
